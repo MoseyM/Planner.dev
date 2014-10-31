a77fcd6a1975991dc2ebf9eebfa7411cb9368310
Margaret <Queen@Big-Macs-MacBook-Pro-149.local>
2014-10-31 10:22:50 -0500

    Completed code for debugging on arrays being printed with no value.

--- public/address_book.php
+++ public/address_book.php
@@ -1,3 +1,5 @@
+<!-- NEED TO DO THE FOLLOWING: 1. FIX SYNTAX ON IF/ELSE(CHK) 2. GET ARRAY TO COMBINE AND PRINT OUT ON SCREEN 3. ADD DELETE KEY TO ADDRESS BOOK 4. UPDATE VARIABLES SO ADDRESSES IS USED APPROPRIATELY.-->
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -5,51 +7,77 @@
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
 		<title> Address Book </title>
-		<? 
-		// Use a CSV file to save to your list after each valid entry.
-		$addresses = [
-		    ['The White House', '1600 Pennsylvania Avenue NW', 'Washington', 'DC', '20500'],
-		    ['Marvel Comics', 'P.O. Box 1527', 'Long Island City', 'NY', '11101'],
-		    ['LucasArts', 'P.O. Box 29901', 'San Francisco', 'CA', '94129-0901']
-		];		
+		<?php 
+		// Use a CSV file to save to your list after each valid entry	
+		$addresses = read();
+		function read() {
+			$fileLocation = './csv/addressBook.csv';
+			$handle = fopen($fileLocation, 'r');
+			$addressBook = [];
+			//feof will put our pointer at the end of the file. so this condition checks to make sure we are not at the end.
+			while (!feof($handle)) {
+				//setting a variable equal to each line of the csv file
+				$row = fgetcsv($handle);
+				//below we are chking to make sure the row is not empty. and if not we are adding it $row to the array declared above $addressBook
+				$addressBook[] = $row;
+				if (is_null($row)) {
+				}
+			}
+			fclose($handle);
+			return $addressBook;
+		}
+
 		function saveAddress($addresses) {
 			$handle=fopen('/vagrant/sites/planner.dev/public/csv/addressBook.csv', 'w');
+			
 			foreach ($addresses as $addressArray => $value) {
-				fputcsv($handle, $value);
+				if(is_array($value)) {
+					//writes to the file
+					fputcsv($handle, $value);
+				}
 			}
 			fclose($handle);
 		}
 
 			//check for a valid entry here...if/else
 		function validityOfEntry() {
-			if (isset($_POST)) {
-				$inputIncluded = 0;
+			$keysChecked = 0;
+			if (!empty($_POST)) {
 				foreach ($_POST as $key => $value) {
-					if(strlen($_POST[$key])>=1) {
-						$inputIncluded++;
+					if($key =='phone'){
 					}
-				}
-					if ($inputIncluded >= 5) {
-						return $_POST;
+					elseif ((bool)$_POST[$key] == false) {
+						echo "<p>Please enter your $key<p>";
 					}
 					else {
-						echo "Missing Information. Please enter all required information";
+						++$keysChecked;
 					}
+				}
+			if($keysChecked >= 5) {
+				return $_POST;
 			}
 		}
+	}
+
+		//we are starting with what was saved in addressBook.csv, each time we reload.
 		
-		//function to update the table with new adresses
-		// function updateAddressList($addresses) {
-		// 	foreach ($addresses as $key => $value) {
-		// 		foreach ($value as $word) {
-		// 			echo $word;
-		// 		}
-		// 	}
-		// }
+		if (isset($_GET['id'])) {
+					$index = $_GET['id'];
+					unset($addresses[$index]);
+					saveAddress($addresses);
+		}
+
+		//$newAddress is the new entry we collected by selecting "CompleteAddress" so we will only append to $addresses if pass validityOfEntry function.
+		$newAddress = validityOfEntry();
+		if ($newAddress == $_POST) {
+			$addresses[] = $newAddress;
+			saveAddress($addresses);
+			
+		}
 		?>
 	</head>
-
 	<body class="container-fluid">
+
 	<!-- Created table to output the information submitted through the form for the address book -->
 		<table class="table table-striped">
 			<tr>
@@ -59,28 +87,22 @@
 				<th>State</th>
 				<th>Zip</th>
 				<th>Phone</th>
+				<th>Delete?</th>
 			</tr>
 
-			<tr>
-
-				<? if (validityOfEntry()):
-					$addresses[] = validityOfEntry();
-					foreach ($addresses as $key => $address): ?>
-					<tr>
-					<!-- once we move forward can remove this and just have the nested foreach -->
-						<? if(is_array($address)):
-							foreach ($address as $value): ?>
-								<td> <?= $value; ?> </td>
-						<? endforeach 
-						;
-						else: ?>
-							<td> <?= $address; ?> </td>
-							<? endif; ?>
-					</tr>
-				<? endforeach; 
-				endif; 
-				saveAddress($addresses);	
-				?>
+			<?php
+				foreach ($addresses as $key => $address) {
+					if(is_array($address) && !false && !null) {
+						echo "<tr>";
+							foreach ($address as $key2 => $value) {
+								echo "<td> $value </td>";
+							}
+							//To add an X at the end of each row to delete the row
+							echo "<td><a href=\"?id=$key\"> &#88; </a></td>";
+						echo "</tr>";
+					}
+				}
+			?>
 		</table>
 	
 		<!-- Create a form that will ask for name, address, city, state, zip, and phone. -->
@@ -117,9 +139,9 @@
 
 			<div class="form-group">
 				<input type="submit" value="CompleteAddress">
-			</div> 
+			</div>
 		</form>
-
+	</div> 
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</body>
 
